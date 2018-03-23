54b9a5df1a99d9341ff242c59ea955f612eca293
Tuomas Suutari <tuomas.suutari@anders.fi>
2018-03-23 14:54:52 +0200

    Improve structure of the tests
    This makes bare "pytest" work for running the tests.
    
    The test settings are now in a separate file, so they are more cleanly
    separated (and not tied to a runner script).
    
    There is also some unrelated fixes to tox.ini.
    
    There is no real need for a wrapper script anymore, but a bigger project
    might need one later, so therefore I added a bare runtests script that
    runs tox.  More functionality can be added to that later, if something
    is required.

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/settings.py
@@ -0,0 +1,42 @@
+TEMPLATES=[
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.admin',
+    'django.contrib.sessions',
+    '{{ cookiecutter.project_slug }}',
+    '{{ cookiecutter.project_slug }}.tests.testapp',
+]
+
+ROOT_URLCONF = []
